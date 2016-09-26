ca1b0cfa38162bb6c796162dfd4e7d9519626362
Sven Albert-Pedersen <svalpe@gmail.com>
2016-09-26 12:47:41 +0200

    edit viewer class

--- src/main/java/bachelor/MapSimulation.java
+++ src/main/java/bachelor/MapSimulation.java
@@ -3,6 +3,7 @@ import javax.swing.JFrame;
 
 import org.jxmapviewer.JXMapViewer;
 import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.VirtualEarthTileFactoryInfo;
 import org.jxmapviewer.viewer.DefaultTileFactory;
 import org.jxmapviewer.viewer.GeoPosition;
 import org.jxmapviewer.viewer.TileFactoryInfo;
@@ -13,8 +14,9 @@ public class MapSimulation {
 		JXMapViewer mapViewer = new JXMapViewer();
 
 		// Create a TileFactoryInfo for OpenStreetMap
-		TileFactoryInfo info = new OSMTileFactoryInfo();
-		DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+		TileFactoryInfo osmInfo = new OSMTileFactoryInfo();
+		TileFactoryInfo veInfo = new VirtualEarthTileFactoryInfo(VirtualEarthTileFactoryInfo.MAP);
+		DefaultTileFactory tileFactory = new DefaultTileFactory(osmInfo);
 		mapViewer.setTileFactory(tileFactory);
 		
 		// Use 8 threads in parallel to load the tiles
@@ -24,7 +26,7 @@ public class MapSimulation {
 		GeoPosition magicRoundabout = new GeoPosition(51.5628550,-1.7715011);
 		
 
-		mapViewer.setZoom(100);
+		mapViewer.setZoom(1);
 		mapViewer.setAddressLocation(magicRoundabout);
 		
 		// Display the viewer in a JFrame
