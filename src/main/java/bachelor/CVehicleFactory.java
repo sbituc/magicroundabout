9915b2a62adcc54d1330a599c379c328b5c8d366
Sven Albert-Pedersen <svalpe@gmail.com>
2016-10-07 09:33:14 +0200

    added folders car + map

---
+++ src/main/java/bachelor/CVehicleFactory.java
@@ -0,0 +1,18 @@
+package bachelor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class CVehicleFactory implements IVehicleFactory {
+    protected List<IVehicle> vehicles = new ArrayList<IVehicle>();
+
+    public CVehicleFactory() {
+        this.generateVehicles();
+    }
+
+    public List<IVehicle> getVehicles() {
+        return vehicles;
+    }
+
+    protected abstract void generateVehicles();
+}
